94c8c419c78684927887e49e4323d7446f5f7df8
DOHODOOO <91409420+DOHODOOO@users.noreply.github.com>
2022-12-24 12:56:26 +0300

    Add files via upload

---
+++ def.py
@@ -0,0 +1,28 @@
+def ABOBA(ABABA):
+    for BABA in["0","1","2","3","4","5","6","7","8","9","\\","+","-","=","/","*"]:
+        for fack in str(ABABA):
+            if BABA == fack:
+               print("ERROR")
+               ABABA=input("Введите пароль:")
+ABABA=input("Введите пароль:")
+ABOBA(ABABA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
